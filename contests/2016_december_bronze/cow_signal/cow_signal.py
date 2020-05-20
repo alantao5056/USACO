041552b99333d56ec318b2b637618ac05cbc3553
alantao5056 <alantao5056@outlook.com>
2020-05-20 14:05:03 -0400

    finished cow_signal

--- contests/2016_december_bronze/cow_signal/cow_signal.py
+++ contests/2016_december_bronze/cow_signal/cow_signal.py
@@ -1,5 +1,28 @@
+def enlarge(line: list, K: int) -> list:
+  firstEle = line[0]
+  newLine = []
+  curChar = firstEle
+  for e in range(0, K):
+    newLine.append(firstEle)
+  for i in range(1, len(line)):
+    curEle = line[i]
+    if curEle == curChar:
+      for j in range(0, K):
+        newLine.append(curEle)
+    else:
+      curChar = curEle
+      for l in range(0, K):
+        newLine.append(curEle)
+  return newLine
+
+
 def amplifySignal(signal: list) -> list:
-  return []
+  newList = []
+  for line in signal:
+    enlargedList = enlarge(line, K)
+    for k in range(0, K):
+      newList.append(enlargedList)
+  return newList
 
 input = open('cowsignal.in', 'r')
 M, N, K = [int(x) for x in input.readline().split()]
@@ -9,6 +32,8 @@ for _ in range(M):
 input.close()
 
 amplifiedSignal = amplifySignal(signal)
+print(amplifiedSignal)
 output = open('cowsignal.out', 'w')
-output.writelines(amplifiedSignal)
-output.close
+for line in amplifiedSignal:
+  output.write("".join(line) + "\n")
+output.close()
